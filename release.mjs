c6e44cf1e63b5ee5e34586c53e8ac4f6e4b847f9
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2024-08-17 20:02:41 +0200

    chore(release): v0.1.1

--- release.mjs
+++ release.mjs
@@ -1,5 +1,6 @@
-import { execSync } from "node:child_process";
+import { exec } from "node:child_process";
 import fs from "node:fs";
+import { promisify } from 'node:util'
 
 const pkg = fs.readFileSync("package.json");
 const packageJSON = JSON.parse(pkg);
@@ -9,6 +10,11 @@ packageJSON.devDependencies = undefined;
 
 fs.writeFileSync("package.json", JSON.stringify(packageJSON, undefined, "\t"));
 
-execSync("pnpm pack");
+const execPromise = promisify(exec);
+
+await execPromise("pnpm pack");
+const { stdout } =  await execPromise("pnpm publish --no-git-checks")
+
+console.log(stdout);
 
 fs.writeFileSync("package.json", pkg);
