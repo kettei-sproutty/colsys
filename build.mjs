7d8eed6d59c872304060bec61f3deb7813e0d213
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2024-08-17 18:51:18 +0200

    feat(build): add cjs and types (#4)
    * feat: add cjs and types
    
    * feat: add main.cjs
    * feat: add main.d.ts
    * feat: update package.json exports
    
    * feat: add build
    
    * fix(build): use node for imports
    
    * chore(build): remove console log
    
    * fix(types): add call signature
    
    * fix: ignore tgz

---
+++ build.mjs
@@ -0,0 +1,14 @@
+import { execSync } from "node:child_process";
+import fs from "node:fs";
+
+const pkg = fs.readFileSync("package.json");
+const packageJSON = JSON.parse(pkg);
+
+packageJSON.scripts = undefined;
+packageJSON.devDependencies = undefined;
+
+fs.writeFileSync("package.json", JSON.stringify(packageJSON, undefined, "\t"));
+
+execSync("pnpm pack");
+
+fs.writeFileSync("package.json", pkg);
---
+++ src/main.cjs
@@ -0,0 +1,18 @@
+const util = require("node:util");
+
+const handler = {
+	get(target, property) {
+		const properties = target?.()?.properties || [];
+		properties.push(property);
+		return new Proxy(() => ({ properties }), handler);
+	},
+
+	apply(target, _thisArg, args) {
+		const properties = target?.()?.properties;
+		return util.styleText(properties, args.join(" "));
+	},
+};
+
+const nodeSyles = new Proxy(() => {}, handler);
+
+module.exports = nodeSyles;
