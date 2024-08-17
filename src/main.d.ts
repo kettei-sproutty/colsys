768dabb72c5204dbf0988edfb9dddd32580c6dbc
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2024-08-17 18:16:03 +0200

    feat: add cjs and types
    * feat: add main.cjs
    * feat: add main.d.ts
    * feat: update package.json exports

---
+++ src/main.d.ts
@@ -0,0 +1,15 @@
+import { styleText } from "node:util";
+
+type OmitArray<T> = T extends (infer U)[] ? U : T;
+
+type Params = OmitArray<Parameters<typeof styleText>[0]>;
+
+type NodeStylesProxy = {
+  [key in Params]: NodeStylesProxy;
+  (...args: any[]): string;
+};
+
+declare const nodeStyles: NodeStylesProxy;
+
+export default nodeStyles;
+
