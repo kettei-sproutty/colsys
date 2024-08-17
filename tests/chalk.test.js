2244748d6388ec77f3c343f47d06d9ba9bd2531b
lutaok <lucadig.1994@gmail.com>
2024-08-17 18:04:40 +0200

    chore: apply linting fixes

--- tests/chalk.test.js
+++ tests/chalk.test.js
@@ -1,11 +1,11 @@
-import nodeStyles from "../src/main.mjs";
 import chalk from "chalk";
-import { expect, test, describe } from "vitest";
+import { describe, expect, test } from "vitest";
+import nodeStyles from "../src/main.mjs";
 
 describe("basic chalk compatibility", () => {
 	test("it can combine styled and normal strings", () => {
-		const ns = nodeStyles.blue("Hello") + " World" + nodeStyles.red("!");
-		const c = chalk.blue("Hello") + " World" + chalk.red("!");
+		const ns = `${nodeStyles.blue("Hello")} World${nodeStyles.red("!")}`;
+		const c = `${chalk.blue("Hello")} World${chalk.red("!")}`;
 		expect(ns).toBe(c);
 	});
 
@@ -24,22 +24,18 @@ describe("basic chalk compatibility", () => {
 	test("nest styles", () => {
 		const ns = nodeStyles.red(
 			"Hello",
-			nodeStyles.underline.bgBlue("world") + "!",
+			`${nodeStyles.underline.bgBlue("world")}!`,
 		);
-		const c = chalk.red("Hello", chalk.underline.bgBlue("world") + "!");
+		const c = chalk.red("Hello", `${chalk.underline.bgBlue("world")}!`);
 		expect(ns).toBe(c);
 	});
 
 	test("nest styles of the same type even (color, underline, background)", () => {
 		const ns = nodeStyles.green(
-			"I am a green line " +
-				nodeStyles.blue.underline.bold("with a blue substring") +
-				" that becomes green again!",
+			`I am a green line ${nodeStyles.blue.underline.bold("with a blue substring")} that becomes green again!`,
 		);
 		const c = chalk.green(
-			"I am a green line " +
-				chalk.blue.underline.bold("with a blue substring") +
-				" that becomes green again!",
+			`I am a green line ${chalk.blue.underline.bold("with a blue substring")} that becomes green again!`,
 		);
 
 		expect(ns).toBe(c);
